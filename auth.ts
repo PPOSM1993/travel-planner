08a1cb7e6c70c032f096b5b30a73bbd8b46ccac4
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2025-11-04 15:32:23 -0300

    Se inicia y configura el proyecto de la app de planificacion de viajes

---
+++ auth.ts
@@ -0,0 +1,15 @@
+// auth.ts
+import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "@/lib/prisma"
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
+})
